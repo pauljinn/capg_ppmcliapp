3106f1df1cbf7c2594b5d043c10114bd79ae07c6
code-gram <pankajsharma.yash@gmail.com>
2020-11-30 16:43:11 +0530

    Header and ProjectItem Component decorated with UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
 import HeaderComponent from './components/layout/HeaderComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,24 @@ import ProjectItemComponent from './ProjectItemComponent';
 class Dashboard extends React.Component {
     render() {
         return (
-            <ProjectItemComponent/>
+            <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                            Create a Project
+                        </a>
+                        <br />
+                        <hr />
+    
+                       <ProjectItemComponent/>
+    
+                    </div>
+                </div>
+            </div>
+        </div>
         )
     }
 }
