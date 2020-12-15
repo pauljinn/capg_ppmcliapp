af189347f7defa9e6c3a4ebbc0a08402ab168536
code-gram <pankajsharma.yash@gmail.com>
2020-12-15 12:52:56 +0530

    Controlled Component-AddForm onChange done

--- src/components/projects/AddProject.js
+++ src/components/projects/AddProject.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
 
 class AddProject extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            projectName:"",
+            projectIdentifier:"",
+            description:"",
+            start_date:"",
+            end_date:""
+        }
+        //this.onChange=this.onChange.bind(this);
+    }
+
+    onChange=(event)=>{
+       this.setState(
+           {[event.target.name]:event.target.value}
+       );
+    }
     render() {
         return (
             <div className="project">
@@ -11,23 +29,49 @@ class AddProject extends Component {
                         <hr />
                         <form>
                             <div className="form-group">
-                                <input type="text" className="form-control form-control-lg " placeholder="Project Name" />
+                                <input 
+                                    type="text" 
+                                    className="form-control form-control-lg " 
+                                    placeholder="Project Name" 
+                                    name="projectName" 
+                                    onChange={this.onChange}
+                                    value={this.state.projectName}/>
                             </div>
                             <div className="form-group">
-                                <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID"
+                                <input 
+                                    type="text" 
+                                    className="form-control form-control-lg" 
+                                    placeholder="Unique Project ID" 
+                                    name="projectIdentifier" 
+                                    onChange={this.onChange}
+                                    value={this.state.projectIdentifier}
                                     />
                             </div>
                            
                             <div className="form-group">
-                                <textarea className="form-control form-control-lg" placeholder="Project Description"></textarea>
+                                <textarea 
+                                    className="form-control form-control-lg" 
+                                    placeholder="Project Description" 
+                                    name="description" 
+                                    onChange={this.onChange}
+                                    value={this.state.description}></textarea>
                             </div>
                             <h6>Start Date</h6>
                             <div className="form-group">
-                                <input type="date" className="form-control form-control-lg" name="start_date" />
+                                <input 
+                                    type="date" 
+                                    className="form-control form-control-lg" 
+                                    name="start_date" 
+                                    onChange={this.onChange}
+                                    value={this.state.start_date}/>
                             </div>
                             <h6>Estimated End Date</h6>
                             <div className="form-group">
-                                <input type="date" className="form-control form-control-lg" name="end_date" />
+                                <input 
+                                    type="date" 
+                                    className="form-control form-control-lg" 
+                                    onChange={this.onChange}
+                                    name="end_date" value={this.state.end_date}/>
                             </div>
     
                             <input type="submit" className="btn btn-primary btn-block mt-4" />
